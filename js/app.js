d7773c7fa3b0efb5515a041ec5ccef43a1469ef1
Adi Erteschik <adi@Adis-MBP.Home>
2015-02-13 17:09:14 +0200

    basic angular with demo first user page

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+var site = angular.module('site', []);//['ngRoute','ui.bootstrap']
+
+// site.config(function($routeProvider) {
+//     $routeProvider
+//         // route for the home page
+//         .when('/', {
+//             templateUrl : 'pages/login.html',
+//             controller  : 'mainController'
+//         });
+// });
+
+// site.config(function ($httpProvider) {
+//     $httpProvider.interceptors.push('authInterceptor');
+// });
+
+// site.run(function(editableOptions) {
+//   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+// });
+
+// angular.element(document).ready(function() {
+//       angular.bootstrap(document, ['site']);
+// });
+
---
+++ js/mainController.js
@@ -0,0 +1,5 @@
+site.controller('mainController',['$scope',function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.image="http://www.ligdol.co.il/Upload/2009715114343_%D7%A1%D7%95%D7%A8%D7%99%D7%A7%D7%98%D7%94.jpg";
+}]);
